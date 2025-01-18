46852c4fbbf92b51b3c00aa06ca31852a4a4bed6
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-01-19 00:40:48 +0600

    Update main.go

--- graphql/main.go
+++ graphql/main.go
@@ -1,15 +1,16 @@
+//go:generate go run github.com/99designs/gqlgen
 package main
 
 import (
 	"log"
 	"net/http"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/handler"
+	"github.com/kelseyhightower/envconfig"
 )
 
 type AppConfig struct {
-	AccountURl string `envconfig:"ACCOUNT_SERVICE_URL"`
+	AccountURL string `envconfig:"ACCOUNT_SERVICE_URL"`
 	CatalogURL string `envconfig:"CATALOG_SERVICE_URL"`
 	OrderURL   string `envconfig:"ORDER_SERVICE_URL"`
 }
@@ -21,12 +22,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	s, err := NewGraphQLServer(cfg.AccountURl, cfg.CatalogURL, cfg.OrderURL)
+	s, err := NewGraphQLServer(cfg.AccountURL, cfg.CatalogURL, cfg.OrderURL)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	http.Handle("/graphql", handler.GraphQL(s.ToExecutableSchema()))
-	http.Handle("/playground", playground.Handler("schlafer", "/graphql"))
+	http.Handle("/playground", handler.Playground("schlafer", "/graphql"))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
